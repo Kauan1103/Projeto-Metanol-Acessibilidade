6e673e2bac1763d273e6505f4cef72ec2a12b53d
Kauan1103 <00001103297016sp@al.educacao.sp.gov.br>
2025-10-06 23:40:13 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,12 @@
+// Funções de acessibilidade
+function aumentarFonte() {
+  document.body.style.fontSize = 'larger';
+}
+
+function diminuirFonte() {
+  document.body.style.fontSize = 'smaller';
+}
+
+function alternarContraste() {
+  document.body.classList.toggle('alto-contraste');
+}
